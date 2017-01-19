e5c78c127e19f77424beaf901b5d74a5eb2925ad
klemensowski <klemensowski@gmail.com>
2017-01-19 09:53:38 -0500

    Added Existing

---
+++ js/docReady.js
@@ -0,0 +1,10 @@
+// Barts html5 starter kit
+$(document).ready(function(){
+var windowWidth=$(window).width();windowWidth>=960?(pc=!0,tablet=!1,phone=!1):480>=windowWidth?(pc=!1,tablet=!1,phone=!0):(pc=!1,tablet=!0,phone=!1);
+	
+        // Background
+    $('[data-background]').each(function(index, element) {
+        var backgroundSrc = $(this).data('background');
+        $(this).backstretch(backgroundSrc);
+    });
+});
