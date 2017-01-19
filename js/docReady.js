bd57a9f78802d6d5081224f349d5dd67cba81ade
klemensowski <eyesaura@outlook.com>
2017-01-19 11:04:39 -0500

    File Clean Up

--- js/docReady.js
+++ js/docReady.js
@@ -1,6 +1,6 @@
 // Barts html5 starter kit
 $(document).ready(function(){
-var windowWidth=$(window).width();windowWidth>=960?(pc=!0,tablet=!1,phone=!1):480>=windowWidth?(pc=!1,tablet=!1,phone=!0):(pc=!1,tablet=!0,phone=!1);
+var windowWidth=$(window).width();windowWidth>=1200?(pc=!0,tablet=!1,phone=!1):480>=windowWidth?(pc=!1,tablet=!1,phone=!0):(pc=!1,tablet=!0,phone=!1);
 	
         // Background
     $('[data-background]').each(function(index, element) {
